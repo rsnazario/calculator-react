5f491b2660f7081f11f2a28df21c395bec73fcc5
rsnazario <rafael.s.nazario@gmail.com>
2020-11-25 18:21:31 -0300

    Removed Unnecessary Files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1>Hello World</h1>
+  </div>,
   document.getElementById('root')
 );
 
